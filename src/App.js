8619ae1343a1faf99604ca33c368be7fcb24a688
Sakib Rahman <seu.sakib@gmail.com>
2022-03-25 23:08:13 +0600

    add git header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Shop from "./component/Shop/Shop";
+import Header from "./component/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Shop></Shop>
     </div>
   );
---
+++ src/component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <h1>Richi Rich Car Shop</h1>
+    </nav>
+  );
+};
+
+export default Header;
