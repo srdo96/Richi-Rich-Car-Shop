47a8bebaa2d77877e36864880e62b369c073f9fa
Sakib Rahman <seu.sakib@gmail.com>
2022-03-26 00:24:54 +0600

    add cards

--- src/component/Car/Car.js
+++ src/component/Car/Car.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Car.css";
 const Car = ({ car }) => {
-  console.log("inside car", car);
+  const { img, name, price } = car;
   return (
     <div className="car">
-      <img src={car.img} alt="" />
+      <img src={img} alt="" />
+      <div className="car-info">
+        <h3>{name}</h3>
+        <p>Price: $ {price}</p>
+      </div>
+      <button className="add-cart-btn">
+        <p>Add to Cart</p>
+      </button>
     </div>
   );
 };
--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Car from "../Car/Car";
-
+import Cart from "../Cart/Cart";
+import "./Shop.css";
 const Shop = () => {
   const [cars, setCars] = useState([]);
   useEffect(() => {
@@ -10,10 +11,15 @@ const Shop = () => {
   }, []);
 
   return (
-    <div>
-      {cars.map((car) => (
-        <Car key={car.id} car={car}></Car>
-      ))}
+    <div className="shop">
+      <div className="container">
+        {cars.map((car) => (
+          <Car key={car.id} car={car}></Car>
+        ))}
+      </div>
+      <div className="cart">
+        <Cart></Cart>
+      </div>
     </div>
   );
 };
